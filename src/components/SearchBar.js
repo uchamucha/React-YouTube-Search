c3c1d22e97f4b8b685e046ace387b4d8beb116d7
Usamath PC <usamathpallikkachalil@gmail.com>
2020-06-29 17:42:02 +0530

    Feat: Updates to SearchBar Component 1. Added form control 2. Made input controllable using state and onChange event-handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  //initialize state
+  state = {
+    term: "",
+  };
+
+  //function to control what happens when input changes
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
   render() {
-    return <h2>SearchBar</h2>;
+    return (
+      <div>
+        <form className="ui form container">
+          <h4>Search YouTube</h4>
+          <div className="ui action input">
+            {/* make input controllable using value, state and onChange event handling */}
+            <input
+              value={this.state.term}
+              onChange={this.onInputChange}
+              type="text"
+              placeholder="Search..."
+            />
+            <button className="ui button">Search</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
