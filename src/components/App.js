5490bf6f4ea0c734481eb8760f90e4a68739cbca
Usamath PC <usamathpallikkachalil@gmail.com>
2020-06-29 17:11:51 +0530

    First Commit-CleanUp-Basic Scaffolding
    1. Added index.js
    2. Added App and SearchBar components
    3. Linked Semantic UI CSS

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>APP</h1>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return <h2>SearchBar</h2>;
+  }
+}
+
+export default SearchBar;
