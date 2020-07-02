618444315bc0fdaef436fffdf4383284f6f1e6d0
Usamath PC <usamathpallikkachalil@gmail.com>
2020-07-02 15:10:15 +0530

    Feat: Created VideoItem and VideoList components

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = (props) => {
+  return (
+    <div>
+      <VideoItem />
+    </div>
+  );
+};
+
+export default VideoList;
