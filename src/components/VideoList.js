802b2663caf5926b72c917929b062967ef2d1b95
Usamath PC <usamathpallikkachalil@gmail.com>
2020-07-02 15:35:57 +0530

    Fix: SetState and VideoList
    1. Corrected setState function
    2. Added prop to VideoList

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,12 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
-  return (
-    <div>
-      <VideoItem />
-    </div>
-  );
+  //process the information before returning
+
+  const renderedList = props.videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
