579634304fb2c242514b9c8b602e01befe4ea06e
Usamath PC <usamathpallikkachalil@gmail.com>
2020-07-02 13:30:36 +0530

    Feat: Added function to be sent as SearchBar prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+const KEY = "AIzaSyDCK8UiqK-3eXB6krQhwrmEolljdLn4ti0";
 
 class App extends React.Component {
+  //define search function to be sent as prop into SearchBar component
+  onTermSubmit = (term) => {
+    youtube.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+        type: "video",
+        q: term,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>APP</h1>
-        <SearchBar />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
