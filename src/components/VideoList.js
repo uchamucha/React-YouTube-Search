003a0ca66502d78118b4dfdc341ae8aff235b6f7
Usamath PC <usamathpallikkachalil@gmail.com>
2020-07-03 12:04:10 +0530

    Feat: Added logic to render videoList components

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,12 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
-  return (
-    <div>
-      <VideoItem />
-    </div>
-  );
+  //render videos into list before rendering it
+  const renderedList = props.videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
