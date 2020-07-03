1ff4675d7acf68f6c002b9ccff6d989bf27895be
Usamath PC <usamathpallikkachalil@gmail.com>
2020-07-03 13:04:59 +0530

    Feat: Render title and thumbnail

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const VideoItem = (props) => {
-  return <div>VideoItem</div>;
+  console.log(props);
+  return (
+    <div>
+      <img src={props.video.snippet.thumbnails.medium.url} />
+      {props.video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
